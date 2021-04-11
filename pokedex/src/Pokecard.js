9e4c5b819b9cfc9a04de2ed77da27ba2a4aa98fa
Camilla Murgia <c.murgia4@studenti.uniss.it>
2021-04-11 20:02:00 +0200

    add some style and add the fancy assets for pokemon image

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import './Pokecard.css'
-const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`
+
+let padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3) : num);
 
 class Pokecard extends Component {
     render() {
-        let imgSrc = `${POKE_API}${this.props.id}.png`
+        let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
         return (
             <div className="Pokecard">
-                <h2>{this.props.name}</h2>
-                <img src={imgSrc} alt={this.props.name}/>
-                <p>Type: {this.props.type}</p>
-                <p>EXP: {this.props.exp}</p>
-                
+                <h2 className="Pokecard-title">{this.props.name}</h2>
+                <img src={imgSrc} alt={this.props.name} className="Pokecard-image"/>
+                <p className="Pokecard-type">Type: {this.props.type}</p>
+                <p className="Pokecard-exp">EXP: {this.props.exp}</p>               
             </div>
         )
     }

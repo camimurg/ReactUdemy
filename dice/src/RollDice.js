0a40285637bf2603c5b75a50a85e8187c7c3bebd
Camilla Murgia <camillamurgia@3522GLOBAL.local>
2021-05-10 09:18:38 +0200

    create die component

---
+++ dice/src/RollDice.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class RollDice extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default RollDice;

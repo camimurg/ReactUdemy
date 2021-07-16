ca49e0fe25eadbab6aaa98bc949a8c26cd5c0f3d
Camilla Murgia <camillamurgia@3522GLOBAL.local>
2021-07-16 11:24:41 +0200

    starting project

---
+++ vending-machine/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <VendingMachine />
+      <Chips />
+      <Sardines />
+      <Soda />
+    </div>
+  );
+}
+
+export default App;
